57ae303562f2b4a50ba9e21de5c454cbedb22880
awesoee <141790208+awesoee@users.noreply.github.com>
2024-09-22 19:57:56 -0400

    Soup

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+import { IPluginConfig } from '@fraytools/plugin-core/lib/types';
+import { ILibraryAssetMetadata } from '@fraytools/plugin-core/lib/types/fraytools';
+
+
+export interface LittleAlchemy2Config extends IPluginConfig {
+  gameURL:string;
+}
